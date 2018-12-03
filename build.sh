862c38e0e3a4a9d571ec102c2b0086a40ad2f57b
Evan Hegyi <evan@evanhegyi.com>
2018-12-03 11:26:29 -0500

    Pushed to hybris repo

--- build.sh
+++ build.sh
@@ -5,8 +5,12 @@ GOARCH="amd64" \
 GOHOSTARCH="amd64" \
 GOHOSTOS="linux" \
 GOOS="linux" \
+VERSION="v2" \
+APP_NAME="sample"
+PORT="5005"
 go build hello-world.go
-docker build -t repository.hybris.com:5006/upsurge/sample:latest . 
-# docker push repository.hybris.com:5006/upsurge/sample:latest
+docker build -t repository.hybris.com:${PORT}/upsurge/${APP_NAME}:latest . 
+docker tag repository.hybris.com:${PORT}/upsurge/${APP_NAME}:latest repository.hybris.com:${PORT}/upsurge/${APP_NAME}:${VERSION}
+docker push repository.hybris.com:${PORT}/upsurge/${APP_NAME}:${VERSION}
 
-docker run --rm --name sample repository.hybris.com:5006/upsurge/sample:latest
+# docker run --rm --name sample repository.hybris.com:${PORT}/upsurge/${APP_NAME}:${VERSION}
