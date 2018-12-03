85b6663da57bba3d672394242f2402c39944f7f9
Jonathan Hess <jonathan.hess@sap.com>
2018-12-03 09:42:15 -0700

    Adds kubernetes deployment

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ GOHOSTARCH="amd64" \
 GOHOSTOS="linux" \
 GOOS="linux" \
 go build hello-world.go
-docker build -t repository.hybris.com:5006/upsurge/sample:latest . 
-# docker push repository.hybris.com:5006/upsurge/sample:latest
 
-docker run --rm --name sample repository.hybris.com:5006/upsurge/sample:latest
+docker build -t repository.hybris.com:5005/upsurge/sample:latest .
+docker push repository.hybris.com:5005/upsurge/sample:latest
+
+#docker run --rm --name sample repository.hybris.com:5005/upsurge/sample:latest
