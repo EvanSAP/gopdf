2cce0d91df65b5a3c73f2653af261b278f6e0915
Jonathan Hess <jonathan.hess@sap.com>
2018-12-03 12:24:06 -0700

    Fixing go build for docker.

--- build.sh
+++ build.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+set -euxo
+
 export GO111MODULE=on
 export GOPATH=$HOME/go
 
 go get github.com/gin-gonic/gin
 go get github.com/golang/protobuf
 
-GOARCH="amd64" \
-GOHOSTARCH="amd64" \
-GOHOSTOS="linux" \
-GOOS="linux" \
-VERSION="v4" \
 APP_NAME="gopdf"
 PORT="5005"
+VERSION="v4"
+
 rm $APP_NAME | echo "File ${APP_NAME} does not exist. Don't care!"
-go build $APP_NAME.go
-docker build -t repository.hybris.com:${PORT}/upsurge/${APP_NAME}:latest . 
+
+GOARCH="amd64" GOHOSTARCH="amd64" GOHOSTOS="linux"  GOOS="linux" go build $APP_NAME.go
+
+
+docker build -t repository.hybris.com:${PORT}/upsurge/${APP_NAME}:latest .
 docker tag repository.hybris.com:${PORT}/upsurge/${APP_NAME}:latest repository.hybris.com:${PORT}/upsurge/${APP_NAME}:${VERSION}
 # docker push repository.hybris.com:${PORT}/upsurge/${APP_NAME}:${VERSION}
 
